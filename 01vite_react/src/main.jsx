a6a012363501245073f2d2e1836276df32743cca
pranavyamjal <yamjal0188@gmail.com>
2024-06-26 18:17:58 +0530

    Custom React in Vite

--- 01vite_react/src/main.jsx
+++ 01vite_react/src/main.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Children } from 'react'
 
+
+function myApp()
+{
+  return(
+    <div>
+      <h1> Custom App</h1>
+    </div>
+  )
+}
+
+//  const ReactElement = {
+//       type: 'a',
+//       props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//       },
+
+//       children: 'Click me to visit google'
+//  }
+
+
+const anotherElement = (
+  
+  < a href= "https://google.com" target='_blank'>
+    Vist Google
+  
+  </a>
+
+)
+
+const anotherUser = "chai aur code"
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: 'https://google.com', target: '_blank'
+  },
+  'Click me to visit Google',
+  
+  anotherElement
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  reactElement
 )
