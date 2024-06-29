f1eecf194b95d07e8cfd042ae953ffcf97862233
pranavyamjal <yamjal0188@gmail.com>
2024-06-29 18:55:39 +0530

    Password Generator (useCallback, useEffect, useRef)

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useCallback, useEffect, useRef } from 'react';
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  // useRef is used to create a mutable object that persists for the lifetime of the component.
+  // Initial value is null. Here, it will be used to reference the password input field.
+  const passwordRef = useRef(null);
+
+  /*
+   * The passwordGenerator function will generate a new password based on the current state.
+   * We know that this function will always be called whenever `numberAllowed`, `length`, `charAllowed`,
+   * or `setPassword` changes. Calling this function every time these dependencies change can be inefficient.
+   *
+   * To optimize performance and avoid unnecessary recalculations, we can use the `useCallback` hook from React.
+   * The `useCallback` hook will return a memoized version of the function that only changes if one of the dependencies has changed.
+   * This ensures that the function is not recreated unnecessarily and helps improve the efficiency of the application.
+   */
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  // This function copies the generated password to the clipboard.
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  // useEffect runs the passwordGenerator function once after the initial render and whenever the dependencies change.
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  return (
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className="text-white text-center my-3">Password Generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />
+        <button
+          onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-700 focus:bg-blue-900 active:bg-blue-900 text-white px-3 py-0.5 shrink-0 "
+        >
+          Copy
+        </button>
+      </div>
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => setLength(e.target.value)}
+          />
+          <label>Length: {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => setNumberAllowed((prev) => !prev)}
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="characterInput"
+            onChange={() => setCharAllowed((prev) => !prev)}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
