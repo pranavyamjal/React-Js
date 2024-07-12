a412fbfb1e889915337b027bff868c120156ab51
pranavyamjal <yamjal0188@gmail.com>
2024-07-12 21:00:29 +0530

    Light/Dark Mode Project through context api

---
+++ 09themeSwitcher/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  darkMode: "class",  // for using dark and light modes in tailwind
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
